2ed39930a2dbdfe3f0d7e5e591e42e2ede390170
Jonathon Bowring <jbowring@informatica.com>
2021-04-21 21:08:09 +1000

    Styling is getting there. More polish required.

--- frontend/mam-frontend/src/App.js
+++ frontend/mam-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import InfaLogo from './infa-logo.svg';
 import { MediaModel } from './MediaModel';
 
 class App extends React.Component {
@@ -10,8 +11,10 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.fileInput = React.createRef();
     this.state = {
-      medias: []
-      
+      medias: [],
+      selectedMedia: null,
+      popoutVisible: false,
+      popoutClass: 'app-aside-hidden'
     }
   }
   componentDidMount() {
@@ -22,14 +25,48 @@ class App extends React.Component {
     .then((response) => {
       let newMedias = this.state.medias;
       response.data._embedded.medias.map((data, i) => {
-        newMedias.push(new MediaModel(data));
+        return newMedias.push(new MediaModel(data));
       });
       this.setState( {medias: newMedias });
-      console.log(this.state.medias);
     });
   }
   
+  closePopout() {
+    this.setState({ 
+      'popoutClass': 'app-aside-hidden', 
+      'popoutVisible': false 
+    });
+  }
 
+  selectMedia(index, event) {
+    
+    let media = this.state.medias[index];
+
+    let popoutClass = '';
+    let popoutVisible = false;
+    
+    if(!this.state.popoutVisible) {
+      
+      popoutClass = 'app-aside-visible';
+      popoutVisible = true;
+
+      this.setState({ 
+        'popoutClass': popoutClass, 
+        'popoutVisible': popoutVisible,
+        'selectedMedia': media 
+      });
+
+    }
+    else {
+
+      this.setState({ 
+        'selectedMedia': media 
+      });
+
+    }
+
+  }
+  
 
   deleteRow(id, e){  
     axios.delete(`http://localhost:8080/media/${id}`)  
@@ -76,7 +113,7 @@ class App extends React.Component {
      
      <div className="App">
         <header className="app-header">
-          <h1>Header</h1>
+          <img className="infaLogo" src={ InfaLogo } alt="Informatica"/>
         </header>
         <nav className="app-nav">
           <h1>Nav</h1>
@@ -93,7 +130,7 @@ class App extends React.Component {
             <div className="app-content-inner">
               {
                   this.state.medias.map((media, index) => {
-                    return <img src={ media.url} className="grid-image" />
+                    return <img key={ media.id } src={ media.url} alt={ media.fileName } className="grid-image" onClick={ (event) => this.selectMedia(index, event) } />
                   })
               }
             </div>
@@ -103,8 +140,38 @@ class App extends React.Component {
         <footer className="app-footer">
           <h1>Footer</h1>
         </footer>
-        <aside className="app-aside">
-          <h1>Aside</h1>
+        <aside className={ this.state.popoutClass }>
+          <span className="closeButton" onClick={ () => this.closePopout() }>&times;</span>
+          <form className="popForm">
+            <div>
+              <label className="popLabel">ID:</label>
+              <input className="popInput" type="text" id="popId" name="popId" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.id : '' }/>
+            </div>
+            <div>
+              <label className="popLabel">File Name:</label>
+              <input className="popInput" type="text" id="popFilename" name="popFilename" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.fileName : '' }/>
+            </div>
+            <div>
+              <label className="popLabel">File Extension:</label>
+              <input className="popInput" type="text" id="popFileExtension" name="popFileExtension" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.fileExtension : '' }/>
+            </div>
+            <div>
+              <label className="popLabel">File Encoding:</label>
+              <input className="popInput" type="text" id="popFileEncoding" name="popFileEncoding" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.fileEncoding : '' }/>
+            </div>
+            <div>
+              <label className="popLabel">File Size:</label>
+              <input className="popInput" type="text" id="popFileSize" name="popFileSize" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.fileSize : '' }/>
+            </div>
+            <div>
+              <label className="popLabel">Mime Type:</label>
+              <input className="popInput" type="text" id="popMimeType" name="popMimeType" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.mimeType : '' }/>
+            </div>
+            <div>
+              <label className="popLabel">URL:</label>
+              <input className="popInput" type="text" id="popURL" name="popURL" defaultValue={ this.state.selectedMedia != null ? this.state.selectedMedia.url : '' }/>
+            </div>
+          </form>
         </aside>
       </div>
     ); // End return
